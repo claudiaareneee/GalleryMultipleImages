b8b79a2c5fcfe3bfd935a2e2d3d74e40970a92f4
claudiaareneee <claudiaareneee@gmail.com>
2018-07-06 16:00:30 -0500

    In this commit, I added the functionality of being able to upload multiple images to a server.

--- app/src/main/java/com/example/claudia/gallerymultipleimages/MainActivity.java
+++ app/src/main/java/com/example/claudia/gallerymultipleimages/MainActivity.java
@@ -1,73 +1,92 @@
 package com.example.claudia.gallerymultipleimages;
 
-//   Context context1 = getBaseContext(); //App context and Base context were the same?
-//        System.out.println("THE BASE CONTEXT IS " + context1.getExternalFilesDir(null));
-
-//Button chooseBtn = (Button)findViewById(R.id.chooseBtn);
-//        Button uploadBtn = (Button)findViewById(R.id.uploadBtn);
-
-//import android.content.Context;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import com.asksira.bsimagepicker.BSImagePicker;
 import com.bumptech.glide.Glide;
 
-import java.util.List;
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+import net.gotev.uploadservice.UploadService;
+
+import static net.gotev.uploadservice.UploadService.*;
 
 public class MainActivity extends AppCompatActivity implements BSImagePicker.OnMultiImageSelectedListener{
 
     private ImageView ivImage1, ivImage2, ivImage3, ivImage4, ivImage5, ivImage6;
-    private Button selectBtn, uploadBtn;
+    private EditText editTextName;
+
+    public List<Uri> uriPaths = new ArrayList<>();
+    public boolean uriPathsFull = false;
+
+    private static final String UPLOAD_URL ="http://192.168.64.2/UploadExample/upload.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //server demo
+        UploadService.NAMESPACE = BuildConfig.APPLICATION_ID;
+
+
+        Button selectBtn, uploadBtn;
+        //multiple image demo
         ivImage1 = findViewById(R.id.iv_image1);
         ivImage2 = findViewById(R.id.iv_image2);
         ivImage3 = findViewById(R.id.iv_image3);
         ivImage4 = findViewById(R.id.iv_image4);
         ivImage5 = findViewById(R.id.iv_image5);
         ivImage6 = findViewById(R.id.iv_image6);
+        selectBtn = findViewById(R.id.selectBtn);
+        uploadBtn = findViewById(R.id.uploadBtn);
+        editTextName = findViewById(R.id.editTextName);
 
-        selectBtn = (Button)findViewById(R.id.selectBtn);
-        uploadBtn = (Button)findViewById(R.id.uploadBtn);
-
+        //This is the file path
         System.out.println("\n\n************************");
         Context context = getBaseContext();
         System.out.println(context.getFilesDir());
 
+        //asynchronous method
         selectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 BSImagePicker pickerDialog = new BSImagePicker.Builder("com.asksira.imagepickersheetdemo.fileprovider")
                         .setMaximumDisplayingImages(Integer.MAX_VALUE)
                         .isMultiSelect()
-                        .setMinimumMultiSelectCount(2)
-                        .setMaximumMultiSelectCount(6)
+                        .setMinimumMultiSelectCount(1)
+                        .setMaximumMultiSelectCount(3)
                         .build();
                 pickerDialog.show(getSupportFragmentManager(), "picker");
-
             }
         });
 
+        //asynchronous method
         uploadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Do Something
+                for (int j = 0; j < uriPaths.size(); j++){
+                    uploadImage(j);
+                }
             }
         });
-
     }
-
+    //Multiple images
     @Override
     public void onMultiImageSelected(List<Uri> uriList) {
+        uriPaths.clear();
+        uriPathsFull = false;
         for (int i=0; i < uriList.size(); i++) {
             if (i >= 6) return;
             ImageView iv;
@@ -91,12 +110,38 @@ public class MainActivity extends AppCompatActivity implements BSImagePicker.OnM
                 default:
                     iv = ivImage6;
             }
-            System.out.println("\n\n************************");
-            System.out.println(uriList.get(i).getPath());
-            System.out.println("\n\n************************");
+            String path = uriList.get(i).toString();
+            path = path.substring(path.lastIndexOf("//")+1);
+            System.out.println("*\n******\n" + path + "\n******\n");
 
+            uriPaths.add(Uri.parse(path));
             Glide.with(this).load(uriList.get(i)).into(iv);
         }
-        uploadBtn.setVisibility(View.VISIBLE);
+        uriPathsFull = true;
+        System.out.println("\n\nThe uriPaths are: " + uriPaths + "\n\n*****");
     }
+
+    private void uploadImage(int i){
+        String name = editTextName.getText().toString().trim();
+        String path = null;
+        try{
+            path = uriPaths.get(i).toString();
+        }catch (NullPointerException e){
+            Toast.makeText(this,"There are no files here!!", Toast.LENGTH_LONG).show();
+            System.out.println("There are no files here!!!!!");
+        }
+        try{
+            String uploadId = UUID.randomUUID().toString();
+            new MultipartUploadRequest(this,uploadId,UPLOAD_URL)
+                .addFileToUpload(path,"image")
+                .addParameter("name", name)
+                .setNotificationConfig(new UploadNotificationConfig())
+                .setMaxRetries(2)
+                .startUpload();
+            Toast.makeText(this,"Uploading !!", Toast.LENGTH_SHORT).show();
+        }catch (Exception e){
+            Toast.makeText(this,"Upload failed :(", Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
