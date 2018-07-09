809a3dbef3ba6ccfc3c00df9b14d0c0b87fcc84a
Claudia Nelson <claudiaareneee@gmail.com>
2018-07-09 15:08:58 -0500

    Update sandcone.php
    I added the other necessary SFM files

--- phpscripts/sandcone.php
+++ phpscripts/sandcone.php
@@ -9,13 +9,32 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         try{
             echo getcwd() . "<br>";
+
+            copy("./SFM_core_files/makeList.bat",        "./uploads/".$name."/makeList.bat");
+            copy("./SFM_core_files/denseRecon_batch.bat","./uploads/".$name."/denseRecon_batch.bat");
+            copy("./SFM_core_files/denseRecon.vbs",      "./uploads/".$name."/denseRecon.vbs");
+            copy("./SFM_core_files/sparseRecon64.bat",   "./uploads/".$name."/sparseRecon64.bat");
+            copy("./SFM_core_files/clean_folder.bat",    "./uploads/".$name."/clean_folder.bat");
+
+
+            echo getcwd() . "<br>";
             chdir('uploads/'.$name);
             echo getcwd() . "<br>";
 
-            //shell_exec('makeList.bat');
+            echo "starting makeList.bat";
             system("makeList.bat");
+            
+            echo "starting sparseRecon64.bat";
+            set_time_limit(90);
+            system("sparseRecon64.bat");
+
+            echo "starting denseRecon.vbs";
+            set_time_limit(90);
+            system("denseRecon.vbs");
+
             chdir('../..');
             echo getcwd() . "<br>";
+            $response['error'] = false; 
 
         }catch(Exception $e){
             $response['error'] = false; 
@@ -26,4 +45,5 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $response['message'] = 'please choose a file';
     }
     echo json_encode($response);
+    echo "<br>SUCCESS!";
 }
